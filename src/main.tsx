169bbc4f8d9e01d91fd80d0a445f063830118aae
Aigars Uplejs <aigars.uplejs@gmail.com>
2022-03-09 21:20:46 +0200

    Add graphql integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const uri = import.meta.env.VITE_API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client} >
+          <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
