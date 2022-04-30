6778964c0422cdcb14a01d67ec7cc8233d2d2237
Aigars Uplejs <aigars.uplejs@gmail.com>
2022-04-30 07:53:41 +0300

    Update loader

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import classes from './loader.module.scss';
 
 const Loader = () => {
     return (
-        <div
-            style={{ borderTopColor: 'transparent' }}
-            className="w-24 h-24 border-4 border-cream border-solid rounded-full animate-spin"
-        />
+        // <div
+        //     style={{ borderTopColor: 'transparent' }}
+        //     className="w-24 h-24 border-4 border-cream border-solid rounded-full animate-spin"
+        // />
+        <div className="flex flex-col content-center items-center">
+            <div className={classes.ldsEllipsis}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="font-color-cream italic font-extrabold opacity-80">
+                Notiek ielāde
+            </div>
+        </div>
+
     );
 };
 
